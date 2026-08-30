63f11f42ec83c79bcd82e284f155faae44756c25
tanmoybiswas478 <sranit55@gmail.com>
2026-08-31 02:36:04 +0530

    upgrade

--- config/cors.php
+++ config/cors.php
@@ -1,25 +1,19 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
     */
-
+    
+    // Yahan hum API routes ko allow kar rahe hain
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*','http://localhost:4200'],
+    // '*' matlab kisi bhi frontend (localhost ya live) se request aa sakti hai
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
@@ -30,5 +24,4 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => false,
-
 ];
