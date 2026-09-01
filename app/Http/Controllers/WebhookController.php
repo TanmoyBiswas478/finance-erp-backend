0b00e3b1f9f905739dfd1161a36c4acd6882a7d7
tanmoybiswas478 <sranit55@gmail.com>
2026-09-01 15:12:20 +0530

    controller & seeder update

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -50,7 +50,7 @@ class WebhookController extends Controller
                       "{\"amount\": float, \"type\": \"EXPENSE\" or \"INCOME\", \"bank_name\": \"Federal Bank\" or \"Bandhan\" or \"HDFC\" or \"Slice\" or \"Slice CC\" or \"Utkarsh SuperMoney\" or \"Bandhan CC\", \"category\": \"Food & Shopping\" or \"Shopping\" or \"Travel\" or \"Bills & Utilities\" or \"Income / Cashback\" or \"Other Expenses\"}";
 
             try {
-                $response = Http::timeout(10)->post("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={$apiKey}", [
+                $response = Http::timeout(10)->post("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={$apiKey}", [
                     'contents' => [
                         [
                             'parts' => [
